276597a52e27a064039cdb5806c7d80951bf4713
schathuranga <schathurangajayaz@outlook.com>
2017-09-10 17:28:40 +0530

    recorrected fxml file locations

--- src/main/java/com/geekz/pos/ui/views/Main.java
+++ src/main/java/com/geekz/pos/ui/views/Main.java
@@ -1,5 +1,12 @@
 package com.geekz.pos.ui.views;
 
+import com.geekz.orm.GeekzORM;
+import com.geekz.orm.ORMFactory;
+import com.geekz.pos.controller.*;
+import com.geekz.pos.model.Client;
+import com.geekz.pos.model.ClientOrder;
+import com.geekz.pos.model.ItemRepo;
+import com.geekz.pos.model.Teller;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -62,5 +69,26 @@ public class Main extends Application
     public static void main(String[] args)
     {
         launch(args);
+        GeekzORM orm = ORMFactory.getORM("hibernate");
+
+        TellerController teller = new TellerController();
+        teller.addNewTeller("Milinda", "0713264601");
+        Teller dbTeller = teller.readTeller(1);
+
+        ClientController client = new ClientController();
+        client.addNewClient("Ama", "13/B 3rd lane", "21212", "2013/12/31");
+        Client dbclient = client.readTeller(1);
+
+        ClientOrderController order = new ClientOrderController();
+        order.addNewOrder("2014/1/11", "", "pending", dbTeller, dbclient);
+        ClientOrder orderdb = order.readClientOrder(1);
+
+        ItemRepoController item =  new ItemRepoController();
+        item.addNewItem("Chili", "kg", "kg", "23.50", "2014/12/12");
+        ItemRepo itemDb = item.readRepoItem(1);
+
+        ItemOrderController itemOrder = new ItemOrderController();
+        itemOrder.addNewItemOrder(10.00, 7.00, orderdb, itemDb);
+        System.out.println();
     }
 }
